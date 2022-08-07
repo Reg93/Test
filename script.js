897d851bb3839f3c9aaa172ec4e69cbe1c7c95d6
reg33 <antilopa33git@gmail.com>
2022-08-07 23:37:13 +0700

    todoist display:block

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
-const tabs = document.querySelector('.tabs')
+const tabs = document.querySelector('.tabs');
+let todoList = document.querySelector('.todo-list');
 
 const login = document.querySelector(".login");
 document.querySelector('.login__button').addEventListener('click', function(){
     tabs.removeChild(login);
+    todoList.style.display = 'block';
+
 })
 
-const listBox = document.querySelector('.list-box')
-const checkbox = document.createElement('input');
+let listItems = document.querySelector('.list__items')
+let checkbox = document.createElement('input');
 
 document.querySelector('.list__button').onclick = () => {
     checkbox.type = 'checkbox';
+    checkbox.style.marginTop = '30px';
+    checkbox.style.width = '25px';
+    checkbox.style.height = '25px';
 
-    listBox.appendChild(checkbox);
+    listItems.appendChild(checkbox);
 }
 
-listBox.onclick = () => {
+listItems.onclick = () => {
     if (checkbox.checked){
-        listBox.removeChild(checkbox);
+        listItems.removeChild(checkbox);
     }
     else return;
 }
